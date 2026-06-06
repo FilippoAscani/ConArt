8d3f9cb0e5a2c5c0709683c700883b636e24f409
Filippo <filippo.ascani@hotmail.it>
2026-06-06 20:02:08 +0200

    miglioramento test 2

---
+++ src/main/java/demo/conart/bean/BeanShow.java
@@ -0,0 +1,35 @@
+package demo.conart.bean;
+
+public class BeanShow {
+
+
+    private int requestId;
+    private int showId;
+    private int artistId;
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
+
+    public int getShowId() {
+        return showId;
+    }
+
+    public void setShowId(int showId) {
+        this.showId = showId;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+
+
+}
